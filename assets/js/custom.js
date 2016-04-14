c4e77ecba7c3f617c12f0cf0b29009704a3aa094
pkshyamkrishnan <pkshyamkrishnan@yahoo.in>
2016-04-14 09:51:12 +0530

    post to get

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -217,7 +217,7 @@ $('.svg').each(function(){
 
 			else {
 					 $.ajax({
-							type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+							type        : 'GET', // define the type of HTTP verb we want to use (POST for our form)
 							url         : 'assets/php/contact.php', // the url where we want to POST
 							data        : formData, // our data object
 							dataType    : 'json', // what type of data do we expect back from the server
