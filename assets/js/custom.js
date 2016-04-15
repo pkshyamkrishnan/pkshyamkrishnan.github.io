328264babc2f9304e22db959fe44f2bab12ca98c
pkshyamkrishnan <pkshyamkrishnan@yahoo.in>
2016-04-15 23:22:46 +0530

    wow

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,32 +1,5 @@
 
-$('.svg').each(function(){
-						var $img = jQuery(this);
-						var imgID = $img.attr('id');
-						var imgClass = $img.attr('class');
-						var imgURL = $img.attr('src');
 
-						jQuery.get(imgURL, function(data) {
-								// Get the SVG tag, ignore the rest
-								var $svg = jQuery(data).find('svg');
-
-								// Add replaced image's ID to the new SVG
-								if(typeof imgID !== 'undefined') {
-										$svg = $svg.attr('id', imgID);
-								}
-								// Add replaced image's classes to the new SVG
-								if(typeof imgClass !== 'undefined') {
-										$svg = $svg.attr('class', imgClass+' replaced-svg');
-								}
-
-								// Remove any invalid XML tags as per http://validator.w3.org
-								$svg = $svg.removeAttr('xmlns:a');
-
-								// Replace image with new SVG
-								$img.replaceWith($svg);
-
-						}, 'xml');
-
-				});
 
 
 (function($){
@@ -37,10 +10,44 @@ $('.svg').each(function(){
 
 	$(window).load(function() {
 		$('#status').fadeOut();
+		wow = new WOW({
+			mobile: false
+		});
+		wow.init();
 		$('#preloader').fadeOut('slow');
 	});
-	var loadFilter='.logod';
+
 	$(document).ready(function() {
+		$('.svg').each(function(){
+								var $img = jQuery(this);
+								var imgID = $img.attr('id');
+								var imgClass = $img.attr('class');
+								var imgURL = $img.attr('src');
+
+								jQuery.get(imgURL, function(data) {
+										// Get the SVG tag, ignore the rest
+										var $svg = jQuery(data).find('svg');
+
+										// Add replaced image's ID to the new SVG
+										if(typeof imgID !== 'undefined') {
+												$svg = $svg.attr('id', imgID);
+										}
+										// Add replaced image's classes to the new SVG
+										if(typeof imgClass !== 'undefined') {
+												$svg = $svg.attr('class', imgClass+' replaced-svg');
+										}
+
+										// Remove any invalid XML tags as per http://validator.w3.org
+										$svg = $svg.removeAttr('xmlns:a');
+
+										// Replace image with new SVG
+										$img.replaceWith($svg);
+
+								}, 'xml');
+
+						});
+var loadFilter='.logod';
+
 		$('#Container').mixItUp({
     animation: {
 			duration:750,
@@ -176,10 +183,6 @@ $('.svg').each(function(){
 		 * WOW Animation When You Scroll
 		/* ---------------------------------------------- */
 
-		wow = new WOW({
-			mobile: false
-		});
-		wow.init();
 
 
 		/* ---------------------------------------------- /*
